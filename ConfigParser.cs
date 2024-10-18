57c9513b74bad79213d23b49483db3c948071552
Vyacheslav <imjustafrogger@gmail.com>
2024-10-18 21:35:57 +0300

    Added configuration file

---
+++ ConfigParser.cs
@@ -0,0 +1,116 @@
+﻿using OfficeOpenXml;
+
+namespace TableHelper;
+
+public static partial class ConfigParser
+{
+    public static Config ParseConfig(string inputStr)
+    {
+        inputStr = inputStr.Replace("\r", "");
+        var result = new Config();
+        var split = inputStr.Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+        for (var sectionIndex = 0; sectionIndex < split.Length; sectionIndex++)
+        {
+            var entries = split[sectionIndex]
+                .Split("\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            foreach (var entry in entries)
+            {
+                if (entry.StartsWith('#')) continue;
+
+                if (sectionIndex == 0)
+                {
+                    result.FileNameRules ??= [];
+                    var strings = entry.Split(" -> ");
+                    if (strings.Length is < 2 or > 2)
+                        throw new FormatException("Invalid FileNameRules format");
+
+                    var key = strings[0];
+                    var value = strings[1];
+
+                    if (!result.FileNameRules.TryAdd(key, value))
+                        throw new InvalidDataException("FileNameRules duplicated");
+
+                    continue;
+                }
+
+                if (sectionIndex == 1)
+                {
+                    result.FieldRules ??= [];
+                    var strings = entry.Split(" -> ");
+                    if (strings.Length is < 2 or > 2)
+                        throw new FormatException("Invalid FieldRule format");
+
+                    var key = strings[0];
+                    var value = strings[1];
+
+                    if (!result.FieldRules.TryAdd(key, value))
+                        throw new InvalidDataException("FieldRule duplicated");
+
+                    continue;
+                }
+
+                if (sectionIndex == 2)
+                {
+                    result.FilePaths ??= [];
+
+                    if (entry.EndsWith(".xls"))
+                        throw new FormatException(".xls file format is not supported. Use .xlsx instead.");
+                    if (!entry.EndsWith(".xlsx"))
+                        throw new FormatException($"{entry} file format is not supported. Use .xlsx");
+
+                    if (!result.FilePaths.Add(entry))
+                        throw new InvalidDataException("FilePath duplicated");
+
+                    continue;
+                }
+
+                throw new FormatException($"Invalid section too many lines in config file");
+            }
+        }
+
+        result.CheckValid();
+
+        return result;
+    }
+}
+
+public class Config
+{
+    public Dictionary<string, string> FileNameRules = null!;
+    public Dictionary<string, string> FieldRules = null!;
+    public HashSet<string> FilePaths = null!;
+
+    public void CheckValid()
+    {
+        if (FileNameRules is null) throw new FormatException($"Invalid file name replacement expression");
+        if (FieldRules is null) throw new FormatException($"Invalid field rules expression");
+        if (FilePaths is null) throw new FormatException($"Invalid file paths expression");
+    }
+
+    public Task ProcessAsync() => Task.WhenAll(FilePaths.Select(GetFileModificationTask));
+
+    private async Task GetFileModificationTask(string filePath)
+    {
+        var originalFile = new FileInfo(filePath);
+        using var package = new ExcelPackage(originalFile);
+        var worksheet = package.Workbook.Worksheets.FirstOrDefault(); // Получаем первый лист
+        if (worksheet is null)
+        {
+            Console.WriteLine($"Файл {originalFile} не содержит листов");
+            return;
+        }
+
+        foreach (var pair in FieldRules)
+            worksheet.Cells[pair.Key].Value = pair.Value;
+
+        var resultFileName = originalFile.Name;
+        foreach (var pair in FileNameRules)
+            resultFileName = resultFileName.Replace(pair.Key, pair.Value);
+
+        var resultFilePath = Path.Combine(originalFile.Directory!.FullName, resultFileName);
+        var resultFile = new FileInfo(resultFilePath);
+        await package.SaveAsAsync(resultFile);
+
+        Console.WriteLine($"Изменения сохранены в файле: {resultFilePath}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,130 +1,100 @@
-﻿using System.IO;
+﻿//TODO: result file name control
+//TODO: GUI
+//TODO: Songs replacement
+
 using OfficeOpenXml;
 
-//TODO: result file name control
-//TODO: GUI
+namespace TableHelper;
 
-Console.InputEncoding = System.Text.Encoding.UTF8;
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine(
-    "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2557     \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557    \u2588\u2588\u2557  \u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2557     \u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \n\u255a\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255d\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551     \u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255d    \u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255d\u2588\u2588\u2551     \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255d\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\n   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255d\u2588\u2588\u2551     \u2588\u2588\u2588\u2588\u2588\u2557      \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2551     \u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255d\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255d\n   \u2588\u2588\u2551   \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551     \u2588\u2588\u2554\u2550\u2550\u255d      \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u255d  \u2588\u2588\u2551     \u2588\u2588\u2554\u2550\u2550\u2550\u255d \u2588\u2588\u2554\u2550\u2550\u255d  \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\n   \u2588\u2588\u2551   \u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255d\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557    \u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2551     \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2551  \u2588\u2588\u2551\n   \u255a\u2550\u255d   \u255a\u2550\u255d  \u255a\u2550\u255d\u255a\u2550\u2550\u2550\u2550\u2550\u255d \u255a\u2550\u2550\u2550\u2550\u2550\u2550\u255d\u255a\u2550\u2550\u2550\u2550\u2550\u2550\u255d    \u255a\u2550\u255d  \u255a\u2550\u255d\u255a\u2550\u2550\u2550\u2550\u2550\u2550\u255d\u255a\u2550\u2550\u2550\u2550\u2550\u2550\u255d\u255a\u2550\u255d     \u255a\u2550\u2550\u2550\u2550\u2550\u2550\u255d\u255a\u2550\u255d  \u255a\u2550\u255d\n                                                                                             \n\n");
-Console.ResetColor();
-Console.WriteLine();
-Console.WriteLine("Изменённые файлы будут сохранены в папке RESULT, она находится в папке с программой");
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("Обратите внимание, что программа работает только с документами с одним листом");
-Console.WriteLine("Обратите внимание, что папка RESULT автоматически очищается");
-Console.ResetColor();
-
-
-var resultFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "RESULT");
-
-if (!Directory.Exists(resultFolderPath))
+file static class Program
 {
-    Directory.CreateDirectory(resultFolderPath);
-    Console.WriteLine("Папка RESULT создана.");
-}
-else
-{
-    await Task.WhenAll(new DirectoryInfo(resultFolderPath).GetFiles().Select(file => Task.Run(file.Delete)));
-    Console.WriteLine("Папка RESULT найдена и очищена.");
-    Console.WriteLine();
-}
+    private static Config _config = null!;
 
-List<string> filePaths = [];
-Console.WriteLine("Введите пути к Excel файлам. Введите 'done', чтобы завершить ввод.");
-while (true)
-{
-    Console.Write("Путь к файлу (или 'done' или клавишу Enter для завершения): ");
-    var filePath = Console.ReadLine();
-
-    if (string.IsNullOrEmpty(filePath) || filePath.ToLower() == "done") break;
-    if (!File.Exists(filePath))
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine("Файл не найден. Попробуйте снова.");
-        continue;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+        PrintLogo();
+        ParseConfig(AskForConfig());
+
+        await _config.ProcessAsync();
+
+        Console.WriteLine();
+        Console.WriteLine("Все операции успешно выполнены");
+        ExitProgram();
     }
 
-    filePaths.Add(filePath);
-    Console.WriteLine($"Файл добавлен: {filePath}");
-}
-
-if (filePaths.Count <= 0)
-{
-    Console.WriteLine("Не выбрано ни одного файла. Завершение программы.");
-    return;
-}
-
-var operations = new Dictionary<string, string>();
-Console.WriteLine("Введите идентификатор ячейки и новое значение. Введите 'done' для завершения ввода.");
-while (true)
-{
-    Console.Write("Идентификатор ячейки (или 'done' или клавишу Enter для завершения): ");
-    var cellId = Console.ReadLine();
-
-    if (string.IsNullOrEmpty(cellId) || cellId.ToLower() == "done") break;
-
-    Console.Write($"Введите новое значение для ячейки {cellId}: ");
-    var newValue = Console.ReadLine();
-    if (string.IsNullOrEmpty(newValue))
+    private static void ParseConfig(string configFilePath)
     {
-        Console.WriteLine("Некорректное новое значение");
-        continue;
+        var sr = new StreamReader(configFilePath);
+        try
+        {
+            _config = ConfigParser.ParseConfig(sr.ReadToEnd());
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[Ошибка] {ex.Message}");
+            Console.ResetColor();
+            ExitProgram();
+        }
+        finally
+        {
+            sr.Close();
+        }
     }
 
-    operations[cellId] = newValue;
-    Console.WriteLine($"Операция добавлена: {cellId} = {newValue}");
-}
-
-if (operations.Count == 0)
-{
-    Console.WriteLine("Не было введено ни одной операции. Завершение программы.");
-    return;
-}
-
-if (!Directory.Exists(resultFolderPath))
-{
-    Directory.CreateDirectory(resultFolderPath);
-    Console.WriteLine("Папка RESULT создана.");
-}
-else
-{
-    await Task.WhenAll(new DirectoryInfo(resultFolderPath).GetFiles().Select(file => Task.Run(file.Delete)));
-    Console.WriteLine("Папка RESULT найдена и очищена.");
-    Console.WriteLine();
-}
-
-await Task.WhenAll(filePaths.Select(GetFileModificationTask).ToArray());
-
-Console.WriteLine();
-Console.WriteLine("Все операции успешно выполнены. Изменённые файлы находятся в папке RESULT.");
-Console.WriteLine();
-Console.WriteLine("Нажмите любую кнопку для закрытия...");
-
-Console.ReadKey();
-Environment.Exit(0);
-
-return;
-
-async Task GetFileModificationTask(string filePath)
-{
-    var originalFile = new FileInfo(filePath);
-    using var package = new ExcelPackage(originalFile);
-    var worksheet = package.Workbook.Worksheets.FirstOrDefault(); // Получаем первый лист
-    if (worksheet is null)
+    private static string AskForConfig()
     {
-        Console.WriteLine($"Файл {originalFile} не содержит листов");
-        return;
+        var configFile = new DirectoryInfo(Environment.CurrentDirectory).GetFiles()
+            .FirstOrDefault(x => x.Extension == ".config");
+        if (configFile is not null) return configFile.FullName;
+
+        Console.WriteLine("Введите пути к файлу настройки. Нажмите Enter для завершения ввода");
+        while (true)
+        {
+            Console.Write("\t> ");
+            var configFilePath = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(configFilePath))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Путь к файлу не был введён");
+                Console.ResetColor();
+                ExitProgram();
+                return string.Empty;
+            }
+
+            if (!File.Exists(configFilePath))
+            {
+                Console.WriteLine("Файл не найден. Попробуйте снова.");
+                continue;
+            }
+
+            return configFilePath;
+        }
     }
 
-    foreach (var operation in operations) worksheet.Cells[operation.Key].Value = operation.Value;
+    private static void ExitProgram()
+    {
+        Console.WriteLine();
+        Console.WriteLine("Нажмите любую кнопку для закрытия...");
 
-    // Сохранение изменённого файла в папку RESULT
-    var resultFilePath = Path.Combine(resultFolderPath, originalFile.Name);
-    var resultFile = new FileInfo(resultFilePath);
-    await package.SaveAsAsync(resultFile); // Асинхронное сохранение
+        Console.ReadKey();
+        Environment.Exit(0);
+    }
 
-    Console.WriteLine($"Изменения сохранены в файле: {resultFilePath}");
+    private static void PrintLogo()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(
+            "\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2557     \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557    \u2588\u2588\u2557  \u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2557     \u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \n\u255a\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255d\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551     \u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255d    \u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255d\u2588\u2588\u2551     \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255d\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\n   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255d\u2588\u2588\u2551     \u2588\u2588\u2588\u2588\u2588\u2557      \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2551     \u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255d\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255d\n   \u2588\u2588\u2551   \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551     \u2588\u2588\u2554\u2550\u2550\u255d      \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u255d  \u2588\u2588\u2551     \u2588\u2588\u2554\u2550\u2550\u2550\u255d \u2588\u2588\u2554\u2550\u2550\u255d  \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\n   \u2588\u2588\u2551   \u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255d\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557    \u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2551     \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2551  \u2588\u2588\u2551\n   \u255a\u2550\u255d   \u255a\u2550\u255d  \u255a\u2550\u255d\u255a\u2550\u2550\u2550\u2550\u2550\u255d \u255a\u2550\u2550\u2550\u2550\u2550\u2550\u255d\u255a\u2550\u2550\u2550\u2550\u2550\u2550\u255d    \u255a\u2550\u255d  \u255a\u2550\u255d\u255a\u2550\u2550\u2550\u2550\u2550\u2550\u255d\u255a\u2550\u2550\u2550\u2550\u2550\u2550\u255d\u255a\u2550\u255d     \u255a\u2550\u2550\u2550\u2550\u2550\u2550\u255d\u255a\u2550\u255d  \u255a\u2550\u255d\n");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Обратите внимание, что программа работает только с документами с одним листом");
+        Console.ResetColor();
+    }
 }
